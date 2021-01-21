8b3647c8721933b5426e3cce8c8d2b43bfa077af
Travis Clarke <travisjonathanclarke@gmail.com>
2021-01-20 23:13:26 -0500

    commented logger

--- app/services/logger_service.py
+++ app/services/logger_service.py
@@ -1,14 +1,14 @@
 """Logstash logger for runnner"""
 
-import logging
-import logstash
-from typing import Dict
+# import logging
+# import logstash
+# from typing import Dict
 
-HOST = 'logstash'
+# HOST = 'logstash'
 
-logger = logging.getLogger('python-logstash-logger')
-logger.setLevel(logging.INFO)
-logger.addHandler(logstash.LogstashHandler(HOST, 5000, version=1))
+# logger = logging.getLogger('python-logstash-logger')
+# logger.setLevel(logging.INFO)
+# logger.addHandler(logstash.LogstashHandler(HOST, 5000, version=1))
 
 # test_logger.addHandler(logstash.TCPLogstashHandler(host, 5959, version=1))
 def log(message: str, extras: Dict = None):
