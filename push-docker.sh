ba6781cd49d0ab945ad16a7a6fa1041740741b87
Travis Clarke <travisjonathanclarke@gmail.com>
2022-04-01 21:56:18 -0400

    added some settings

---
+++ push-docker.sh
@@ -0,0 +1,8 @@
+docker push tclarke104/med-ai-runner:0.1
+
+docker push tclarke104/med-ai-runner-worker:0.1
+docker push tclarke104/med-ai-runner-beat:0.1
+docker push tclarke104/med-ai-runner-results:0.1
+
+docker push tclarke104/med-ai-orthanc:0.1
+docker push tclarke104/med-ai-orthanc-public:0.1
